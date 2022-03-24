848291104822808b8b833c91f656af191263833d
Muhammad Zohaib <zohaibazhar359@gmail.com>
2022-03-24 15:28:41 +0500

    Updated

--- app/Models/VisitorImages.php
+++ app/Models/VisitorImages.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class VisitorImages extends Model
 {
     use HasFactory;
+    public function getImageUrlAttribute(){
+        if($this->images == null){
+            return "";
+        }
+        return "https://square.cybermeteors.com/root/".$this->images;
+    }
 }
