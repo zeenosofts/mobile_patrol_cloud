d4c3f25b3d8b4377f213893bb55d60452087f258
Muhammad Zohaib <zohaibazhar359@gmail.com>
2022-03-29 12:43:02 +0500

    UPdated

--- app/Models/Visitor.php
+++ app/Models/Visitor.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Http\Traits\AttendanceTrait;
 use App\Http\Traits\ScheduleTrait;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -33,14 +34,14 @@ class Visitor extends Model
 
     public function getLocalTimeAttribute(){
         if($this->time_in != null) {
-            return $this->convertDateTimeToDbFormat($this->convertWithRespectToTimeZone($this->time_in, $this->admin_id));
+            return $this->convertDateTimeToDbFormat($this->convertWithRespectToTimeZone(Carbon::parse($this->time_in)->toDateTimeString(), $this->admin_id));
         }else{
             return null;
         }
     }
     public function getLocalTimeOutAttribute(){
         if($this->time_out != null) {
-            return $this->convertDateTimeToDbFormat($this->convertWithRespectToTimeZone($this->time_out, $this->admin_id));
+            return $this->convertDateTimeToDbFormat($this->convertWithRespectToTimeZone(Carbon::parse($this->time_out)->toDateTimeString(), $this->admin_id));
         }else{
             return null;
         }
