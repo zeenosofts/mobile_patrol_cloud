8e66d977d47bc9d8fbbfe6db6a5b7b1dea4918dd
MuazamIftikhar <51464624+MuazamIftikhar@users.noreply.github.com>
2022-05-18 13:40:12 +0500

    time in

--- app/Http/Controllers/Api/AttendanceController.php
+++ app/Http/Controllers/Api/AttendanceController.php
@@ -20,8 +20,7 @@ class AttendanceController extends Controller
 
     public function save_guard_attendance(Request $request){
         try{
-            dd("2");
-            if ($request->schedule_id == ""){
+            if ($request->schedule_id == null){
                 $client_id="";
                 $schedule_id="";
                 $admin_id="";
--- routes/api.php
+++ routes/api.php
@@ -28,6 +28,7 @@ Route::post('/login', [App\Http\Controllers\Api\LoginController::class, 'index']
 
 Route::post('check_guard_time_out',[\App\Http\Controllers\Api\AttendanceController::class,'check_guard_time_out'])->name('check_guard_time_out');
 
+
 //Route::post('/form',[\App\Http\Controllers\Api\LoginController::class,'form'])->name('form')->middleware('auth:api');
 
 Route::middleware(['auth:api','cors'])->group(function () {
@@ -36,6 +37,7 @@ Route::middleware(['auth:api','cors'])->group(function () {
         ->name('get_guard_schedules');
     Route::post('create_guard_attendance',[\App\Http\Controllers\Api\AttendanceController::class,'save_guard_attendance'])
         ->name('create_guard_attendance');
+
     Route::post('get_guard_attendance',[\App\Http\Controllers\Api\AttendanceController::class,'get_guard_attendance'])
         ->name('get_guard_attendance');
     Route::post('save_checkpoint_history',[\App\Http\Controllers\Api\CheckpointController::class,'save_history_checkpoint'])
