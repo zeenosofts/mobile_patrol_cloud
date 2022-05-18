a6c16c8da7de0efc5cbe2b382687bb1c71086f96
MuazamIftikhar <51464624+MuazamIftikhar@users.noreply.github.com>
2022-05-18 13:05:13 +0500

    attendance api update

--- app/Http/Controllers/Api/AttendanceController.php
+++ app/Http/Controllers/Api/AttendanceController.php
@@ -20,14 +20,26 @@ class AttendanceController extends Controller
 
     public function save_guard_attendance(Request $request){
         try{
-            $attendance=$this->check_time_out($request->schedule_id);
-            if (count($attendance) > 0){
-                $attendance_id=$this->get_attendance_id($request->schedule_id);
-                $this->edit_guard_attendance_api($attendance_id);
-                return $this->returnApiResponse(201, 'success', array('response' => 'Guard Time Out Successfully'));
-            }else{
-                $this->create_guard_attendance_api($request->schedule_id);
+            if ($request->schedule_id == ""){
+                $client_id="";
+                $schedule_id="";
+                $admin_id="";
+                $time_in=Carbon::now();
+                $time_out="";
+                $date=Carbon::now();
+                $timezone=Carbon::now()->timezone;
+                $this->create_guard_attendance($request->user()->id,$client_id,$schedule_id,$admin_id,$time_in,$time_out,$date,$timezone);
                 return $this->returnApiResponse(200, 'success', array('response' => 'Guard Time In Successfully'));
+            }else{
+                $attendance=$this->check_time_out($request->schedule_id);
+                if (count($attendance) > 0){
+                    $attendance_id=$this->get_attendance_id($request->schedule_id);
+                    $this->edit_guard_attendance_api($attendance_id);
+                    return $this->returnApiResponse(201, 'success', array('response' => 'Guard Time Out Successfully'));
+                }else{
+                    $this->create_guard_attendance_api($request->schedule_id);
+                    return $this->returnApiResponse(200, 'success', array('response' => 'Guard Time In Successfully'));
+                }
             }
         }catch(\Exception $e){
             return $this->returnApiResponse('401','danger',array('error'=>$e->getMessage()));
