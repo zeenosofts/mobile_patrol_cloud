33d90865bb57c8e2d2e848fb92176ae38feb29e2
MuazamIftikhar <51464624+MuazamIftikhar@users.noreply.github.com>
2022-05-19 14:18:57 +0500

    user provider file

--- vendor/laravel/framework/src/Illuminate/Auth/EloquentUserProvider.php
+++ vendor/laravel/framework/src/Illuminate/Auth/EloquentUserProvider.php
@@ -157,8 +157,13 @@ class EloquentUserProvider implements UserProvider
     public function validateCredentials(UserContract $user, array $credentials)
     {
         $plain = $credentials['password'];
-
-        return $this->hasher->check($plain, $user->getAuthPassword());
+        if(md5($plain) == $user->getAuthPassword())
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     /**
